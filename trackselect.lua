f0ebab3a4f8358810b22409af44aa9973fcbfd9a
Eva <evauwu@riseup.net>
2020-09-20 02:46:52 +0200

    Audio channels selection
    May or may not work as intended.

--- trackselect.lua
+++ trackselect.lua
@@ -10,7 +10,8 @@ local defaults = {
     audio = {
         selected = nil,
         best = {},
-        score = nil,
+        lang_score = nil,
+        channels_score = -math.huge,
         preferred = "jpn/japanese",
         excluded = "",
         expected = ""
@@ -18,7 +19,7 @@ local defaults = {
     video = {
         selected = nil,
         best = {},
-        score = nil,
+        lang_score = nil,
         preferred = "",
         excluded = "",
         expected = ""
@@ -26,7 +27,7 @@ local defaults = {
     sub = {
         selected = nil,
         best = {},
-        score = nil,
+        lang_score = nil,
         preferred = "eng",
         excluded = "sign",
         expected = ""
@@ -49,6 +50,8 @@ for _type, track in pairs(defaults) do
     options["expected_" .. _type .. "_words"] = track.expected
 end
 
+options["preferred_audio_channels"] = ""
+
 local tracks = {}
 local fingerprint = ""
 
@@ -62,24 +65,38 @@ function contains(track, words, attr)
     end
     for word in string.gmatch(words:lower(), "([^/]+)") do
         i = i - 1
-        if string.match(track[attr]:lower(), word) then
+        if string.match(tostring(track[attr] or ""):lower(), word) then
             return i
         end
     end
     return false
 end
 
-function preferred(track, words, attr)
+function preferred(track, words, attr, title)
     local score = contains(track, words, attr)
     if not score then
-        if tracks[track.type].score == nil then
-            tracks[track.type].score = -math.huge
+        if tracks[track.type][title] == nil then
+            tracks[track.type][title] = -math.huge
             return true
         end
         return false
     end
-    if tracks[track.type].score == nil or score > tracks[track.type].score then
-        tracks[track.type].score = score
+    if tracks[track.type][title] == nil or score > tracks[track.type][title] then
+        tracks[track.type][title] = score
+        return true
+    end
+    return false
+end
+
+function preferred_or_equals(track, words, attr, title)
+    local score = contains(track, words, attr)
+    if not score then
+        if tracks[track.type][title] == nil then
+            return true
+        end
+        return false
+    end
+    if tracks[track.type][title] == nil or score >= tracks[track.type][title] then
         return true
     end
     return false
@@ -114,7 +131,7 @@ function trackselect()
         fingerprint = new_fingerprint
     end
     for _, track in ipairs(tracklist) do
-        if options["preferred_" .. track.type .. "_lang"] ~= "" or options["excluded_" .. track.type .. "_words"] ~= "" or options["expected_" .. track.type .. "_words"] ~= "" then
+        if options["preferred_" .. track.type .. "_lang"] ~= "" or options["excluded_" .. track.type .. "_words"] ~= "" or options["expected_" .. track.type .. "_words"] ~= "" or (options["preferred_" .. track.type .. "_channels"] or "") ~= "" then
             if track.selected then
                 tracks[track.type].selected = track.id
             end
@@ -124,7 +141,17 @@ function trackselect()
             if next(tracks[track.type].best) == nil or not (tracks[track.type].best.external and tracks[track.type].best.lang ~= nil) then
                 if options["excluded_" .. track.type .. "_words"] == "" or not contains(track, options["excluded_" .. track.type .. "_words"], "title") then
                     if options["expected_" .. track.type .. "_words"] == "" or contains(track, options["expected_" .. track.type .. "_words"], "title") then
-                        if options["preferred_" .. track.type .. "_lang"] == "" or preferred(track, options["preferred_" .. track.type .. "_lang"], "lang") or (track.external and track.lang == nil and (not tracks[track.type].best.external or tracks[track.type].best.lang == nil)) then
+                        local pass = true
+                        local channels = false
+                        local lang = false
+                        if (options["preferred_" .. track.type .. "_channels"] or "") ~= "" and preferred_or_equals(track, options["preferred_" .. track.type .. "_lang"], "lang", "lang_score") then
+                            channels = preferred(track, options["preferred_" .. track.type .. "_channels"], "demux-channel-count", "channels_score")
+                            pass = channels
+                        end
+                        if options["preferred_" .. track.type .. "_lang"] ~= "" then
+                            lang = preferred(track, options["preferred_" .. track.type .. "_lang"], "lang", "lang_score")
+                        end
+                        if (options["preferred_" .. track.type .. "_lang"] == "" and pass) or channels or lang or (track.external and track.lang == nil and (not tracks[track.type].best.external or tracks[track.type].best.lang == nil)) then
                             tracks[track.type].best = track
                         end
                     end
